81fef3977ad6baab4fec8f8061c70acfab79a8e2
AleksandrTurlakov <aturlakov@ukr.net>
2022-10-17 14:30:31 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import markup from './js/templates/markup.hbs';
+
+const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(evt) {
+  const name = evt.target.value.trim();
+  console.log(evt.target.value);
+  fetchCountries(name)
+    .then(showCountries)
+    .catch(err => console.log('Ошибка какой кошмар'));
+  // .then(data.json);
+}
+
+function showCountries(data) {
+  console.log(markup(data.map(country)));
+}
